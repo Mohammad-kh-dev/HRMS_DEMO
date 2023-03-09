bb512137b0ca81c152b03fd0d35fc963f8f806d6
Mohammad Alkhattab <mohammad.alkhattab77@gmail.com>
2023-03-09 17:56:47 +0200

    mm

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Button } from '@mui/material';
 const handleOut = () =>{
  localStorage.removeItem("accessJWT")
